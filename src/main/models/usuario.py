a7083aff11de0db5f0a6df0f4f479d63850a326f
Joaquimfl8 <160670206+Joaquimfl8@users.noreply.github.com>
2026-03-27 11:04:58 -0300

    Update usuario.py

--- src/main/models/usuario.py
+++ src/main/models/usuario.py
@@ -4,6 +4,6 @@ class Usuario(BaseModel):
     id: int = Field(..., gt = 0)
     nome: str = Field(..., min_length = 3)
     gmail: EmailStr
-    telefone: str
+    telefone: str = Field(min_length=11, max_length=11)
     nivelPerm: int = Field(default=1, gt=0, le=5)
     
